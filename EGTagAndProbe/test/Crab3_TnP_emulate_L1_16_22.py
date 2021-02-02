7b3afc9c3dad254e5bfd24fdbef83c5701d46a70
Marko Kovac <mkovac@cern.ch>
2021-02-02 20:01:18 +0100

    First commit.

---
+++ EGTagAndProbe/test/Crab3_TnP_emulate_L1_16_22.py
@@ -0,0 +1,30 @@
+# TEMPLATE used for automatic script submission of multiple datasets
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'TnP_emulate_L1_16_22'
+config.General.workArea = 'Crab'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+#config.JobType.psetName = 'test.py'
+config.JobType.psetName = 'TnP_emulate_L1.py'
+#config.JobType.allowUndistributedCMSSW = True
+config.section_("Data")
+
+config.Data.inputDataset = '/EGamma/Run2018C-PromptReco-v1/MINIAOD'
+config.Data.secondaryInputDataset= '/EGamma/Run2018C-v1/RAW'
+
+config.Data.inputDBS = 'global'
+config.Data.runRange =  '319347'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 15000
+config.Data.totalUnits = -999 # number of event
+config.Data.outLFNDirBase = '/store/user/mkovac/Data/ECAL'
+config.Data.publication = False
+#config.Data.allowNonValidInputDataset = True
+config.Data.outputDatasetTag = 'TnP_emulate_L1_16_22'
+config.section_("Site")
+config.Site.storageSite = 'T3_CH_CERNBOX'
---
+++ EGTagAndProbe/test/Fitter/createCBLib.sh
@@ -0,0 +1,3 @@
+cd include && root -l -b -q loadRooFit.C FuncCB.cpp++
+mv FuncCB_cpp.so FuncCB_cpp_ACLiC_dict_rdict.pcm ../obj
+rm FuncCB_cpp.d
